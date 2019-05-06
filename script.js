1ea38916d9292265f76ad28aee597162deee7c85
Sigrid Stensvold <sigridstens@gmail.com>
2019-05-06 15:42:08 -0500

    Added product gallery functionality that will access an array in the Vue instance.

--- script.js
+++ script.js
@@ -1,22 +1,88 @@
-var openSectionButton = document.querySelector('.nav-icon');
-var modalNav = document.querySelector('.modal-nav');
+//var openSectionButton = document.querySelector('.nav-icon');
+//var modalNav = document.querySelector('.modal-nav');
 
-navButton.addEventListener('click', function() {
-    console.log('open sesame!!!');
+//navButton.addEventListener('click', function() {
+   // if (modalNav !== null) {
+    //    modalNav.style.display = "flex";
+   // }
 
-    if (modalNav !== null) {
-        modalNav.style.display = "flex";
+//});
+
+Vue.component ('inventory-component', {
+    props: ['product'],
+    template: `
+        <figure>
+            <img class="gallery-tile" v-bind:src="product.imgRoute" v-bind:alt="product.name">
+            <figcaption class="product-caption">           
+                <ul>
+                    <li>{{product.name}}</li>
+                    <li>{{product.color}}</li>
+                    <li>{{product.size}}</li>
+                    <li>{{product.price}}</li>
+                </ul>
+            </figcaption> 
+        </figure>
+    `,
+    data: function() {
+        return {inventory: []}
     }
+})
 
-});
-
-
-let vm = new Vue ({
+new Vue ({
     el: "#app",
-    data: {}
-        search: ""
-    },
-    methods: {
+    data: {
+        searchInput: "",
+
+        inventory: [
+            {
+                name: "Levi's Jean Shorts",
+                color: "stone-wash blue",
+                size: 8,
+                price: 12.99,
+                imgRoute: "img/levishorts.jpg",
+                id: 1
+            },
+            {
+                name: "Levi's Jean Shorts",
+                color: "stone-wash blue",
+                size: 8,
+                price: 12.99,
+                imgRoute: "img/levishorts.jpg",
+                id: 2
+            },
+            {
+                name: "Levi's Jean Shorts",
+                color: "stone-wash blue",
+                size: 8,
+                price: 12.99,
+                imgRoute: "img/levishorts.jpg",
+                id: 3
+            },
+            {
+                name: "Levi's Jean Shorts",
+                color: "stone-wash blue",
+                size: 8,
+                price: 12.99,
+                imgRoute: "img/levishorts.jpg",
+                id: 4
+            },
+            {
+                name: "Levi's Jean Shorts",
+                color: "stone-wash blue",
+                size: 8,
+                price: 12.99,
+                imgRoute: "img/levishorts.jpg",
+                id: 5
+            },
+            {
+                name: "Levi's Jean Shorts",
+                color: "stone-wash blue",
+                size: 8,
+                price: 12.99,
+                imgRoute: "img/levishorts.jpg",
+                id: 6
+            },
+        ]
 
     }
 })
