b14fc2df84886eedb4bfe3ed3dab4466bec66311
Sigrid Stensvold <sigridstens@gmail.com>
2019-05-06 15:06:38 -0500

    Added aside for clothing category navigation.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+var openSectionButton = document.querySelector('.nav-icon');
+var modalNav = document.querySelector('.modal-nav');
+
+navButton.addEventListener('click', function() {
+    console.log('open sesame!!!');
+
+    if (modalNav !== null) {
+        modalNav.style.display = "flex";
+    }
+
+});
+
+
 let vm = new Vue ({
     el: "#app",
     data: {}
